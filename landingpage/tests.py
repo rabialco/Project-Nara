ff286c1fdff2c53ec26e9dec770b5a9e2dd64450
Ronaldi Tjaidianto <ronalditjaidianto01@gmail.com>
2019-12-05 07:12:29 +0700

    [RED] Added landingpage views & templateUsed test

--- landingpage/tests.py
+++ landingpage/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase, Client
+from django.urls import resolve
+from .views import landing
 
 # Create your tests here.
 class LandingPageUnitTest(TestCase):
     def test_landing_page_url_is_exist(self):
         response = Client().get('')
         self.assertEqual(response.status_code, 200)
+
+    def test_landing_page_using_landing_views(self):
+        found = resolve('/')
+        self.assertEqual(found.func, landing)
+
+    def test_landing_page_using_template(self):
+        response = Client().get('')
+        self.assertTemplateUsed(response, 'landingpage.html')
