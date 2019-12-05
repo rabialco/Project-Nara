b71b59120795d7bc6515d68ae2801ac677ec5596
Ronaldi Tjaidianto <ronalditjaidianto01@gmail.com>
2019-12-05 20:33:13 +0700

    [RED] Added profile url & views test

--- profil/tests.py
+++ profil/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import profil
 
 # Create your tests here.
+class ProfileUnitTest(TestCase):
+    def test_profile_url_exist(self):
+        response = Client().get('profile/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_using_views(self):
+        found = resolve('/profile/')
+        self.assertEqual(found.func, profil)
