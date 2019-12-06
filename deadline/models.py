b17b53734c28a5ec0b7391488f42dbbf3d2dc556
Rabialco Argana <rabialco@gmail.com>
2019-12-06 14:27:49 +0700

    [GREEN] add forms, templates, urls, models

--- deadline/models.py
+++ deadline/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class dl(models.Model):
+    nama_deadline = models.CharField(max_length=25,blank=True, verbose_name='')
+    selesai = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nama_deadline 
---
+++ deadline/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import index, move, unmove, erase
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<int:pk>', move, name='done'),
+    path('del/<int:pk>', unmove, name='undone'),
+    path('delete/<int:pk>', erase, name='erase')
+]
