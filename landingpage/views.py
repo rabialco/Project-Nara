8f2d087da15a531fbab9bbe837749e3fd03bbcba
Nadia Adilah Rahimullah <nadiaadilahr14@gmail.com>
2019-12-13 12:32:30 +0000

    Login

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 # Create your views here.
+from django.urls import reverse
+
+
 def landing(request):
     return render(request, 'landingpage.html')
 
+
 def tentang(request):
     return render(request, 'tentang.html')
+
+
+def logout(request):
+    """Logs out user"""
+    request.session.flush()
+    return HttpResponseRedirect(reverse('landing'))
