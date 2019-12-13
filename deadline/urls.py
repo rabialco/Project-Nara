d787392e5763d4561992c68af5e6b76d3ba352bd
Rabialco Argana <rabialco@gmail.com>
2019-12-13 22:36:00 +0700

    [GREEN] fix ajax

--- deadline/urls.py
+++ deadline/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, move, unmove, erase
+from .views import index, move, unmove, erase, add_deadline_service
 
 app_name= 'deadline'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('<int:pk>', move, name='done'),
     path('del/<int:pk>', unmove, name='undone'),
-    path('delete/<int:pk>', erase, name='erase')
+    path('delete/<int:pk>', erase, name='erase'),
+    path('add_dl_service/', add_deadline_service, name='add_dl_service')
 ]
