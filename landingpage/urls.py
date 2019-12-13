056ba68f7fae95a40be4d5362e555e5ad7d11fc3
Nadia Adilah Rahimullah <nadiaadilahr14@gmail.com>
2019-12-13 19:13:38 +0700

    [CHORES] add homepage login implementation

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import landing, tentang
+from .views import landing, tentang, logout
 
 urlpatterns = [
     path('', landing, name='landing'),
-    path('about/', tentang, name='tentang')
+    path('about/', tentang, name='tentang'),
+    path('logout/', logout, name='logout'),
 ]
